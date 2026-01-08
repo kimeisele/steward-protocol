f4e6768b407c9d9ace83403fb0e7559d7f2dbf63
kimeisele <kim_eisele@icloud.com>
2026-01-08 16:41:52 +0100

    refactor(fixtures): Pure Gita import pattern + protocol exports
    - fixtures.py: Move RealVibeKernel to top-level (not runtime)
    - universal/__init__.py: Export Acintya model types
    - types.py, read_write.py: Watertight typing updates

--- vibe_core/protocols/universal/__init__.py
+++ vibe_core/protocols/universal/__init__.py
@@ -1,12 +1,25 @@
+from vibe_core.protocols.substrate import MAHAMANTRA_SEQUENCE, HolyName, MantraOpCode, MantraProtocol
+
 from .bhagavan import Bhaga, BhagaTestResult, BhagavanProtocol
 from .enforce import EnforceProtocol
 from .infer import InferProtocol
 from .krishna import IdentityStatus, KrishnaProtocol
-from .mantra import MAHAMANTRA_SEQUENCE, HolyName, MantraOpCode, MantraProtocol
 from .om import OmProtocol
 from .rama import RamaProtocol
 from .ramanujan import RamanujanProtocol
 from .read_write import ReadWriteProtocol
+from .resonance import (
+    AcintyaState,
+    NullResonance,
+    Quality,
+    ResonanceProtocol,
+    SemanticVibration,
+    StructuralIntegrity,
+    SystemAction,
+    create_structure,
+    create_substance,
+    get_quality_from_vector,
+)
 from .store_recall import StoreRecallProtocol
 from .sync import SyncProtocol
 from .types import (
@@ -34,4 +47,4 @@ from .union import EntityStatus, UnionProtocol
 from .yamaraja import DharmaVerdict, YamarajaProtocol
 
 # NOTE: MantraInstruction has been replaced by MantraOpCode from substrate.py
-# Use MantraOpCode (from .mantra or directly from vibe_core.protocols.substrate)
+# Use MantraOpCode (directly from vibe_core.protocols.substrate)
